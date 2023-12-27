273aac52228fe1d9e950743472244907de26000d
Luiz <felyppe.nunes1@gmail.com>
2023-12-27 12:12:47 -0300

    feat: error handling

--- backend/src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ backend/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError";
 import { prisma } from "../../../../lib/prisma";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -14,7 +15,7 @@ export class CreateUserUseCase {
 
         const userExists = await this.usersRepository.findByEmail(normalized_email)
         if (userExists) {
-            throw new Error('User already exists')
+            throw new AppError('User already exists', 400)
         }
 
         const cellphone_ddd = cellphone.substring(0, 2)
