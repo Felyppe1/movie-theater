3e7e86fe54e2d57bdb8b8db7bc41478420a45691
Felyppe <felyppe.nunes1@gmail.com>
2024-02-20 13:38:13 -0300

    feat: types related to room

--- frontend/src/api/rooms.ts
+++ frontend/src/api/rooms.ts
@@ -1,38 +1,43 @@
-import { AddRoomForm } from "@/components/AdminRoomView/useAdminRoomViewForm"
+import { Room, RoomGeneral } from "@/@types/Room"
+import { SeatCreate } from "@/@types/Seat"
+import { Technology } from "@/@types/Technology"
 import { makeRequest } from "@/utils/makeRequest"
 import { QueryFunctionContext } from "@tanstack/react-query"
 
 type CreateRoomProps = {
-  data: AddRoomForm
+  data: Omit<Room, 'id'> & {
+    seats: SeatCreate[],
+    technologyIds: Technology['id'][]
+  }
 }
 
 type UpdateRoomProps = CreateRoomProps & {
-  room_id: string
+  room_id: Room['id']
 }
 
 type DeleteRoomProps = {
-  room_id: string
+  room_id: Room['id']
 }
 
-export const createRoom = async ({ data }: CreateRoomProps) => {
+export const createRoom = async ({ data }: CreateRoomProps): Promise<RoomGeneral> => {
   return await makeRequest('/rooms', {
     method: 'POST',
     data
   })
 }
 
-export const getRoom = async ({ queryKey }: QueryFunctionContext) => {
+export const getRoom = async ({ queryKey }: QueryFunctionContext): Promise<RoomGeneral> => {
   return await makeRequest(`/rooms/${queryKey[1]}`, { method: 'GET' })
 }
 
-export const updateRoom = async ({ data, room_id }: UpdateRoomProps) => {
+export const updateRoom = async ({ data, room_id }: UpdateRoomProps): Promise<RoomGeneral> => {
   return await makeRequest(`/rooms/${room_id}`, {
     method: 'PUT',
     data
   })
 }
 
-export const deleteRoom = async ({ room_id }: DeleteRoomProps) => {
+export const deleteRoom = async ({ room_id }: DeleteRoomProps): Promise<void> => {
   return await makeRequest(`/rooms/${room_id}`,
     { method: 'DELETE' }
   )
--- frontend/src/hooks/api/useGetRoom.ts
+++ frontend/src/hooks/api/useGetRoom.ts
@@ -1,31 +1,12 @@
+import { Room } from "@/@types/Room";
 import { getRoom } from "@/api/rooms";
 import { useQuery } from "@tanstack/react-query";
 
-type Seat = {
-  id: number
-  row: string
-  column: string
-  exists: boolean
-  type: string
-  room_id: string
-}
 
-type Technology = {
-  id: string
-}
-
-type RoomProps = {
-  id: string
-  number: string
-  movie_theater_id: string
-  seats: Seat[]
-  technologies: Technology[]
-}
-
-type UseGetRoomProps = Pick<RoomProps, 'id'>
+type UseGetRoomProps = Pick<Room, 'id'>
 
 export function useGetRoom({ id }: UseGetRoomProps) {
-  return useQuery<RoomProps>({
+  return useQuery({
     queryKey: ['room', id],
     queryFn: getRoom,
     retry: false
