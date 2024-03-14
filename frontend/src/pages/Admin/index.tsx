e0797f5f42dda906524b0e4e9ca9050c92b9158c
Felyppe <felyppe.nunes1@gmail.com>
2024-03-14 16:11:43 -0300

    feat: admin overview text

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -4,7 +4,35 @@ export function Admin() {
   return (
     <>
       <AdminMainHeader h1='Módulo administrativo' p='Gerencie os filmes e cinemas' backLink='' />
-      <h1>Bem vindo, Fulano</h1>
+      <section className='my-5 space-y-4'>
+        <p>
+          Aqui na área administrativa é possível gerenciar todo o conteúdo do site principal. <br/>
+          As permissões são definidas de acordo com as seguintes funções do usuário: <br/>
+        </p>
+        <article>
+          <h2 className='text-lg font-semibold text-secondary-foreground py-1'>Administrador de Filmes</h2>
+          <ul className='list-disc pl-6'>
+            <li>Selecionar filmes para o banco de dados</li>
+          </ul>
+        </article>
+
+        <article>
+          <h2 className='text-lg font-semibold text-secondary-foreground py-1'>Administrador de Cinemas</h2>
+          <ul className='list-disc pl-6'>
+            <li>Cadastrar cinemas</li>
+            <li>Selecionar filmes do banco de dados para exibição no cinema</li>
+            <li>Cadastrar formato das salas de cinema</li>
+            <li>Criar sessões de filmes</li>
+          </ul>
+        </article>
+
+        <article>
+          <h2 className='text-lg font-semibold text-secondary-foreground py-1'>Administrador</h2>
+          <ul className='list-disc pl-6'>
+            <li>Todas as permissões</li>
+          </ul>
+        </article>
+      </section>
     </>
   )
 }
