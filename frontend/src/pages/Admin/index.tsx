7f34db7c22a919231507a85cf8ce6eef22c60e6b
Felyppe <felyppe.nunes1@gmail.com>
2024-03-16 13:19:38 -0300

    refactor: admin message

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,10 +1,25 @@
 import { AdminMainHeader } from "@/components/ui/AdminMainHeader";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { useConfigurationStore } from "@/store/configuration";
+import { FaCircleExclamation } from "react-icons/fa6"
 
 export function Admin() {
+  const configuration = useConfigurationStore(state => state.configuration)
+  
   return (
     <>
       <AdminMainHeader h1='Módulo administrativo' p='Gerencie os filmes e cinemas' backLink='' />
       <section className='my-5 space-y-4'>
+        {configuration?.admin_accessible && (
+          <Alert>
+            <FaCircleExclamation />
+            <AlertTitle>Admin acessível</AlertTitle>
+            <AlertDescription>
+              O módulo administrativo está acessível devido às configurações do administrador
+            </AlertDescription>
+          </Alert>
+        )}
+
         <p>
           Aqui na área administrativa é possível gerenciar todo o conteúdo do site principal. <br/>
           As permissões são definidas de acordo com as seguintes funções do usuário: <br/>
@@ -22,14 +37,15 @@ export function Admin() {
             <li>Cadastrar cinemas</li>
             <li>Selecionar filmes do banco de dados para exibição no cinema</li>
             <li>Cadastrar formato das salas de cinema</li>
-            <li>Criar sessões de filmes</li>
+            <li>Criar sessões de filmes (ainda não implementado)</li>
           </ul>
         </article>
 
         <article>
           <h2 className='text-lg font-semibold text-secondary-foreground py-1'>Administrador</h2>
           <ul className='list-disc pl-6'>
-            <li>Todas as permissões</li>
+            <li>Todas as permissões anteriores</li>
+            <li>Mudar configurações do site</li>
           </ul>
         </article>
       </section>
